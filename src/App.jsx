ba79f4aa22346dafc0f43b9cbf62c8ac79970b2d
chadiboudaher <chadiboudaher7@gmail.com>
2026-04-29 13:17:15 +0300

    Getting started with Dashboard page

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,13 @@ import Dashboard from "./components/layouts/Dashboard";
 import Challenge from "./components/layouts/Challenge";
 
 function App() {
-  return (
-    <Layout>
-      <Welcome />
-      <Dashboard />
-      <Challenge />
-    </Layout>
-  );
+  const selectedPage = 1; // zero for welcome, 1 for dashboard, 2 for challenge
+  const pages = {
+    0: <Welcome />,
+    1: <Dashboard />,
+    2: <Challenge />,
+  };
+  return <Layout>{pages[selectedPage]}</Layout>;
 }
 
 export default App;
--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -1,3 +1,21 @@
 export default function ProgressBar() {
-  return <div></div>;
+  const text = "hello world";
+
+  const arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  return (
+    <div className="level">
+      <div>
+        <h4>{text}</h4>
+      </div>
+
+      {arr.map((element, elementIndex) => {
+        return (
+          <div className="level-bar" key={elementIndex}>
+            {element}
+          </div>
+        );
+      })}
+      <div className="xp" style={{ width: `${40}%` }}></div>
+    </div>
+  );
 }
